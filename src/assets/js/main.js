6ecaf0d762e3029dab87e2a286207673f2d71c13
aephy14 <alejbeck+ae@gmail.com>
2025-12-23 18:43:21 +1300

    Delete src directory

--- src/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const urlParam = new URLSearchParams(window.location.search);
-
-const product = urlParam.get("product")
-var product_name = product ? product : null
-console.log(product_name);
-
-function toggleMenu() {
-    document.getElementById('offcanvas').classList.toggle('active');
-}
